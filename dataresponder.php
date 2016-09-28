941cc25d7c3c714fa9f485301ecdfcdf2582d501
proy251183 <partho.ece@gmail.com>
2016-09-28 00:20:16 -0400

    (first set of Ajax)

---
+++ dataresponder.php
@@ -0,0 +1,15 @@
+<?php
+
+	if ($_GET["data"] == "1"){
+
+	echo(' The value fetched is 1');
+
+	}
+
+	if ($_GET["data"] == "2"){
+
+	echo(' The value fetched is 2');
+
+	}
+
+?>
---
+++ dataresponderpost.php
@@ -0,0 +1,15 @@
+<?php
+
+	if ($_POST["data"] == "1"){
+
+	echo(' The value fetched is yes 1');
+
+	}
+
+	if ($_POST["data"] == "2"){
+
+	echo(' The value fetched is really 2');
+
+	}
+
+?>
